f5f92e7d6b01f0c8adb531e1e2dd2a767f914406
Rishi Verma <rishi.verma21803@gmail.com>
2021-09-12 15:15:11 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template, redirect, request
+import requests
+import json
+
+app = Flask(__name__)
+weather_api = '94f0413c98f83d8fc87048d9dc29b06c'
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/result', methods=['POST', 'GET'])
+def result():
+    if request.method == 'POST':
+
+        # get the country code from restcountries.eu API
+        country_name = request.form['countryname']
+        r = requests.get(f'https://restcountries.eu/rest/v2/name/{country_name}', params={'fields': 'alpha2Code', 'fullText': 'true'})
+        json_data = json.loads(r.text)
+        country_code = json_data[0]['alpha2Code'] 
+
+        # get the weather data from openweather api
+        if request.form['radio'] == 'cityname':
+            city_data = request.form['location']
+            weather_request = requests.get(f'https://api.openweathermap.org/data/2.5/weather?q={city_data}&appid={weather_api}')
+            if weather_request.ok:
+                json_data = json.loads(weather_request.text)
+                return render_template('result.html', data=json_data)
+
+        if request.form['radio'] == 'zip':
+            pass
+
+
+
+if __name__ == ('__main__'):
+    app.run(debug=True)
+
